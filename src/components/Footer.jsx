aee1e9bba4999122ec043424343fde6065c149b4
rickyzatnika <rickyzatnika91@gmail.com>
2024-08-19 20:27:38 +0700

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,8 +16,8 @@ const Footer = () => {
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="md:flex md:items-center md:justify-between">
             <Link prefetch={false} href="/" className="w-fit flex flex-col gap-3 items-start mb-4 sm:mb-0  rtl:space-x-reverse">
-              <Image src="/logo-smk.png" priority={true} width={260} height={160} className="object-contain w-80 h-40" alt="Logo Barland" />
-              <span className="text-xs sm:text-sm text-gray-400 mb-6 md:mb-0" >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, magnam.</span>
+              <Image src="/logo-smk.png" priority={true} width={260} height={160} className="object-contain w-80 h-40" alt="Logo SMK ICB" />
+              <span className="text-xs sm:text-sm text-gray-400 mb-6 md:mb-0" >PROJECT TEST.</span>
             </Link>
             <ul className="flex flex-col gap-3 md:gap-0 md:flex-row flex-wrap items-start md:items-center mb-6 text-sm font-medium text-gray-400 sm:mb-0">
               <li>
@@ -35,7 +35,7 @@ const Footer = () => {
             </ul>
           </div>
           <hr className="my-6 border-lime-500 sm:mx-auto dark:border-gray-700 lg:my-8" />
-          <span className="flex flex-col md:flex-row items-start sm:justify-between text-sm text-gray-400 sm:text-center "><Link href="/" className="hover:underline">© 2024 SMK ICB CINTA NIAGA.</Link>All Rights Reserved.</span>
+          <span className="flex flex-col md:flex-row items-start sm:justify-between text-sm text-gray-400 sm:text-center "><Link href="/" className="hover:underline">© SMK ICB CINTA NIAGA power by CODERS.ID</Link>All Rights Reserved.</span>
         </div>
       </footer>
     </div>
